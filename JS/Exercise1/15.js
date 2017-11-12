9f2faf6edec2188d3750fd67e393d06f24aa62fe
morskibg <dimityrp@yahoo.com>
2017-11-12 20:58:17 +0200

    +

--- JS/Exercise1/15.js
+++ JS/Exercise1/15.js
@@ -1,16 +1,12 @@
 function main(input){
-   let obj = {};
-   for(let kvp of input){
-      let query = kvp.split(' -> ');
-      if(query[0] === 'age' || query[0] === 'grade' ){
-          obj[query[0]] = Number(query[1]);
-      }
-      else{
-          obj[query[0]] = query[1];
-      }
-
-  }
-  let  stringify = JSON.stringify(obj);
-    console.log(stringify);
+    let students = {};
+    input.map(x => x.split(' -> '))
+        .forEach(query => {
+            let key = query[0];
+            let value = isNaN(Number(query[1])) ?
+                query[1] : Number(query[1]);
+                students[key] = value;
+        });
+    console.log(JSON.stringify(students));
 }
 main(['name -> Angel', 'surname -> Georgiev', 'age -> 20', 'grade -> 6.00', 'date -> 23/05/1995', 'town -> Sofia' ])
